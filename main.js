314d5d4f05e43a8ee92533a945457f84a515b961
brownmamba22 <indian.in.smcs@gmail.com>
2015-01-18 06:53:43 -0500

    updated version of the JavaScript which is in the applicant side of the application

--- main.js
+++ main.js
@@ -35,6 +35,31 @@
             description: 'Internship for Facebook.',
             image: 'https://www.facebook.com/images/fb_icon_325x325.png',
             order: 2
+        },
+        {
+            name: 'Google',
+            description: 'Internship for Google.',
+            image: 'http://www.newssetup.com/wp-content/uploads/2014/10/Google-Logo-3.jpg',
+            order: 3
+        },
+        {
+            name: 'Verizon',
+            description: 'Internship for Verizon.',
+            image: 'http://allthingsd.com/files/2011/07/Verizon-logo-big.png',
+            order: 4
+        },
+        {
+            name: 'Comcast',
+            description: 'Internship for Comcast.',
+            image: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Comcast_Logo.svg/2000px-Comcast_Logo.svg.png',
+            order: 5
+        },
+        {
+            name: 'SnapChat',
+            description: 'Internship for SnapChat.',
+            image: 'http://techhive.de/blog/wp-content/uploads/2014/11/snapchat-logo-png.png',
+            order: 6
         }
-    ]
+
+    ];
 })()
